466226b51fd899c9a4b4cd1ff75656a8fb08af76
Bryni eplabaka <brynjolfur19@ru.is>
2021-02-04 10:11:11 +0000

    Init commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+//PART 1 60%
+//TODO Take a word from wordlist
+//TODO Scramble word
+//TODO Show scrambled word
+//TODO Allow user to guess the word
+//TODO Loop until user wants to stop
+
+//PART 2 80%
+//TODO User can ask for a hint
+//TODO The word is shown with dashes, but one random letter is in the correct place.
+//TODO For each hint, one correct letter is added.
+//TODO User starts with 10 points and loses one point for each hint.
+//TODO Implement game rules where 
+//Game is either:
+//How many words can you guess before you have 0 points.
+//or:
+//How many points do you have left after 7 words.
+
+//5% BONUS
+//The time elapsed since the jumble is shown until the answer is correct is also a factor, deducting points.  
+//Find a way to make this work in the overall point calculation.  You can re-invent the entire point calculation if you want.
+
+//PART 3 100%
+//TODO Add time as a factor (see 5% bonus on v.B)
+//TODO Add highscore table
+//TODO For the highscores to be interesting add more ways to calculate scores.  Longer streaks, speed bonuses, etc.
+//TODO Store highscores in file.
+//TODO Allow user to choose to see top 5 scores or full table.
+
+//5-10% BONUS
+// Add extra mode crossword jumble
